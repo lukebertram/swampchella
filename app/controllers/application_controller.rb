c73f615c2adf45f0f4f83637296b3a5c095ae44a
Luke Bertram <10275830+lukebertram@users.noreply.github.com>
2026-05-26 21:48:00 -0700

    hallo swamp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def home
+    render html: "Hello, swamp!"
+  end
 end
